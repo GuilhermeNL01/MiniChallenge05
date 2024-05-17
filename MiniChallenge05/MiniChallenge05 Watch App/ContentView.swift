4f96a8baae695d9d509de06708d904b55b827411
Guilherme Nunes Lobo <loboguilherme2003@gmail.com>
2024-05-17 16:55:08 -0300

    Criação do Projeto no Xcode

---
+++ MiniChallenge05/MiniChallenge05 Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MiniChallenge05 Watch App
+//
+//  Created by Guilherme Nunes Lobo on 17/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MiniChallenge05/MiniChallenge05 Watch App/MiniChallenge05App.swift
@@ -0,0 +1,17 @@
+//
+//  MiniChallenge05App.swift
+//  MiniChallenge05 Watch App
+//
+//  Created by Guilherme Nunes Lobo on 17/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MiniChallenge05_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
