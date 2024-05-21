058c99815bc2a54415879279355aeab1e5aec951
Guilherme Nunes Lobo <loboguilherme2003@gmail.com>
2024-05-21 17:19:00 -0300

    Ajustes

--- MiniChallenge05/MiniChallenge05 Watch App/Components/HealthKitManager.swift
+++ MiniChallenge05/MiniChallenge05 Watch App/Components/HealthKitManager.swift
@@ -5,4 +5,52 @@
 //  Created by Guilherme Nunes Lobo on 21/05/24.
 //
 
-import Foundation
+import HealthKit
+
+class HealthKitManager {
+    let healthStore = HKHealthStore()
+    
+    // Auth do HealthKit
+    func requestAuthorization(completion: @escaping (Bool) -> Void) {
+        // Tipos de dados 
+        guard let distanceType = HKObjectType.quantityType(forIdentifier: .distanceWalkingRunning) else {
+            completion(false)
+            return
+        }
+        
+        let typesToShare: Set<HKSampleType> = []
+        let typesToRead: Set<HKObjectType> = [distanceType]
+        
+        healthStore.requestAuthorization(toShare: typesToShare, read: typesToRead) { success, error in
+            completion(success)
+        }
+    }
+    
+    //  Distância percorrida em metros
+    func fetchWalkingRunningDistance(completion: @escaping (Double) -> Void) {
+        guard let distanceType = HKQuantityType.quantityType(forIdentifier: .distanceWalkingRunning) else {
+            completion(0.0)
+            return
+        }
+        
+        // Intervalo de tempo
+        let startDate = Calendar.current.startOfDay(for: Date())
+        let endDate = Date()
+        let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: .strictStartDate)
+        
+        // soma dos dados
+        let query = HKStatisticsQuery(quantityType: distanceType, quantitySamplePredicate: predicate, options: .cumulativeSum) { _, result, error in
+            var distance: Double = 0
+            
+            if let result = result, let sum = result.sumQuantity() {
+                distance = sum.doubleValue(for: HKUnit.meter())
+            }
+            
+            DispatchQueue.main.async {
+                completion(distance)
+            }
+        }
+        
+        healthStore.execute(query)
+    }
+}
--- MiniChallenge05/MiniChallenge05 Watch App/Views/DistanceView.swift
+++ MiniChallenge05/MiniChallenge05 Watch App/Views/DistanceView.swift
@@ -8,8 +8,74 @@
 import SwiftUI
 
 struct DistanceView: View {
+    @StateObject private var viewModel = ContentViewModel()
+    @State private var isLoading = true
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            if let distance = viewModel.distance {
+                Text("Você andou \(distance, specifier: "%.2f") metros hoje")
+                ProgressBar(value: viewModel.progressPercentage, progressText: "\(Int(viewModel.distance ?? 0))/\(Int(viewModel.dailyGoal)) metros")
+            } else {
+                ProgressView("Carregando...")
+            }
+        }
+        .onAppear {
+            viewModel.requestAuthorization()
+        }
+    }
+}
+
+struct ProgressBar: View {
+    var value: Double
+    var progressText: String
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                Rectangle()
+                    .foregroundColor(Color.gray.opacity(0.3))
+                    .frame(width: geometry.size.width, height: 20)
+                
+                Rectangle()
+                    .foregroundColor(.blue)
+                    .frame(width: min(CGFloat(value) * geometry.size.width, geometry.size.width), height: 20)
+                    .overlay(Text(progressText)
+                                .foregroundColor(.white)
+                                .padding(5))
+            }
+        }
+        .cornerRadius(5.0)
+        .padding(.vertical, 5)
+    }
+}
+
+class ContentViewModel: ObservableObject {
+    @Published var distance: Double?
+    private var healthKitManager = HealthKitManager()
+    let dailyGoal: Double = 1000 // Definindo a meta diária para 1000 metros (altere conforme necessário)
+    
+    var progressPercentage: Double {
+        if let distance = distance {
+            return min(distance / dailyGoal, 1.0)
+        } else {
+            return 0
+        }
+    }
+    
+    func requestAuthorization() {
+        healthKitManager.requestAuthorization { success in
+            if success {
+                self.fetchDistance()
+            } else {
+            }
+        }
+    }
+    
+    private func fetchDistance() {
+        healthKitManager.fetchWalkingRunningDistance { distance in
+            self.distance = distance
+        }
     }
 }
 
