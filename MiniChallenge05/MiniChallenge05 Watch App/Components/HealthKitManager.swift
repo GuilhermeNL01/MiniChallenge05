c73e7e1a1e86264a8e7bd146ac982fd674a3a966
Guilherme Nunes Lobo <loboguilherme2003@gmail.com>
2024-05-21 17:16:52 -0300

    Adicionando o componente do HealthKitManager e a view da DistanceView

---
+++ MiniChallenge05/MiniChallenge05 Watch App/Components/HealthKitManager.swift
@@ -0,0 +1,8 @@
+//
+//  HealthKitManager.swift
+//  MiniChallenge05 Watch App
+//
+//  Created by Guilherme Nunes Lobo on 21/05/24.
+//
+
+import Foundation
---
+++ MiniChallenge05/MiniChallenge05 Watch App/Views/DistanceView.swift
@@ -0,0 +1,18 @@
+//
+//  DistanceView.swift
+//  MiniChallenge05 Watch App
+//
+//  Created by Guilherme Nunes Lobo on 21/05/24.
+//
+
+import SwiftUI
+
+struct DistanceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DistanceView()
+}
