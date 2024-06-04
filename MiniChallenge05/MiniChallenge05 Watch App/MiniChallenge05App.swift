b3a814f46aea56bcc22a001cb920587803861673
Guilherme Nunes Lobo <loboguilherme2003@gmail.com>
2024-06-04 14:51:25 -0300

    Prints retirados e Total de KMs arrumado

--- MiniChallenge05/MiniChallenge05 Watch App/MiniChallenge05App.swift
+++ MiniChallenge05/MiniChallenge05 Watch App/MiniChallenge05App.swift
@@ -13,7 +13,7 @@ struct MiniChallenge05_Watch_AppApp: App {
     
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TotalDistanceComponent()
         }
         .modelContainer(for: ModelNew.self)
     }
